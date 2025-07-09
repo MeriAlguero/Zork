c886305f5ba1abb08986572a2ca0a5ea1b36466f
Meritxell Algueró <merialguerom@gmail.com>
2025-07-09 20:56:26 +0200

    Agregar archivos de proyecto.

---
+++ GameAssigment_MeritxellAlguero/GameAssigment_MeritxellAlguero.cpp
@@ -0,0 +1,17 @@
+// GameAssigment_MeritxellAlguero.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+////#include <conio.h> //getch();
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ReturnPlayer[40];
+    cout << "Welcome to my world!\nBefore we begain heare are some code words:" << endl; //endl o \n es para que haga un espacio
+    cout << "Talk, Walk, Go, Get, Take, Equip, UnEquip, Lock, Unlock, Inventory, Loot, Stats, Attack, Dictionary \n \n Do you want to start? \n Yes or NO" <<endl;
+    cin >> ReturnPlayer;
+
+    return 0;
+}
+
